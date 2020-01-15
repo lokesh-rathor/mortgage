aca88b052e0510319d1d6a1f01660a2105cdb2a3
Rajeev Varshney <RAJEEV.VARSHNEY@NIIT-Tech.com>
2020-01-15 11:59:08 +0530

    rajeev changes

--- src/main/java/com/santander/mortgage/controller/MortgageController.java
+++ src/main/java/com/santander/mortgage/controller/MortgageController.java
@@ -17,23 +17,17 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
 import com.santander.mortgage.dto.ConfirmMortgageResponseDto;
 import com.santander.mortgage.dto.MortgageOptionsResponseDto;
 import com.santander.mortgage.dto.MortgageRequestDto;
 import com.santander.mortgage.dto.MortgageResponseDto;
 import com.santander.mortgage.dto.PropertyDetailsDto;
 import com.santander.mortgage.service.MortgageService;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 import com.santander.mortgage.dto.PaymentDetailsRequestDto;
 import com.santander.mortgage.dto.PaymentDetailsResponseDto;
 import com.santander.mortgage.dto.ValuationRequestDto;
 import com.santander.mortgage.dto.ValuationResponseDto;
 import com.santander.mortgage.exception.InvalidInputException;
-import com.santander.mortgage.service.MortgageService;
 import com.santander.mortgage.service.ValuationService;
 
 @CrossOrigin(origins = "*")
@@ -44,11 +38,6 @@ public class MortgageController {
 	@Autowired
 	private MortgageService mortgageService;
 	private static final Logger logger = LoggerFactory.getLogger(MortgageController.class);
-
-	// @GetMapping("get-data")
-	// public String getData() {
-	// return proxy.sayHello();
-	// }
 
 	@GetMapping("/confirmMortgage/{userId}")
 	public ResponseEntity<ConfirmMortgageResponseDto> confirmMortgage(@PathVariable("userId") Long userId) {
@@ -79,30 +68,35 @@ public class MortgageController {
 
 	}
 
-	  @GetMapping("/propertyDetailsById/{userId}")
-	  public ResponseEntity<PropertyDetailsDto> getPropertyDetailsById(@PathVariable("userId") Long userId) {
-	  PropertyDetailsDto propertyDetailsDto = null;
-	  
-	  try { propertyDetailsDto = mortgageService.getPropertyDetailsById(userId); }
-	  catch (NullPointerException npe) { if (npe.getMessage() == null) {
-	  System.out.println("Error Message : No Record found for this User Id " +
-	  userId); } } return new ResponseEntity<>(propertyDetailsDto, HttpStatus.OK);
-	  }
-	 
-	/*@GetMapping("/confirmMortgage/{userId}")
-	public ResponseEntity<PropertyDetailsDto> confirmMortgage(@PathVariable("userId") Long userId){	
-		PropertyDetailsDto confirmMortgageResponseDto =  mortgageService.confirmMortgage(userId);
-		return new ResponseEntity<>(confirmMortgageResponseDto, HttpStatus.OK);
-	}*/
+	@GetMapping("/propertyDetailsById/{userId}")
+	public ResponseEntity<PropertyDetailsDto> getPropertyDetailsById(@PathVariable("userId") Long userId) {
+		PropertyDetailsDto propertyDetailsDto = null;
 
-	  /*
+		try {
+			propertyDetailsDto = mortgageService.getPropertyDetailsById(userId);
+		} catch (NullPointerException npe) {
+			if (npe.getMessage() == null) {
+				System.out.println("Error Message : No Record found for this User Id " + userId);
+			}
+		}
+		return new ResponseEntity<>(propertyDetailsDto, HttpStatus.OK);
+	}
+
+	/*
 	 * @GetMapping("/confirmMortgage/{userId}") public
-	 * ResponseEntity<PropertyDetailsDto> confirmMortgage(@PathVariable("userId")
-	 * Long userId){ PropertyDetailsDto confirmMortgageResponseDto =
-	 * mortgageService.confirmMortgage(userId); return new
-	 * ResponseEntity<>(confirmMortgageResponseDto, HttpStatus.OK); }
+	 * ResponseEntity<PropertyDetailsDto>
+	 * confirmMortgage(@PathVariable("userId") Long userId){ PropertyDetailsDto
+	 * confirmMortgageResponseDto = mortgageService.confirmMortgage(userId);
+	 * return new ResponseEntity<>(confirmMortgageResponseDto, HttpStatus.OK); }
 	 */
 
+	/*
+	 * @GetMapping("/confirmMortgage/{userId}") public
+	 * ResponseEntity<PropertyDetailsDto>
+	 * confirmMortgage(@PathVariable("userId") Long userId){ PropertyDetailsDto
+	 * confirmMortgageResponseDto = mortgageService.confirmMortgage(userId);
+	 * return new ResponseEntity<>(confirmMortgageResponseDto, HttpStatus.OK); }
+	 */
 
 	// @GetMapping("get-data")
 	// public String getData() {
@@ -122,21 +116,22 @@ public class MortgageController {
 		return new ResponseEntity<>(mortgageOptionsResponseDtoList, HttpStatus.OK);
 	}
 
-//	@Autowired
-//	private RegistrationProxy proxy;
+	// @Autowired
+	// private RegistrationProxy proxy;
 
 	@Autowired
 	private ValuationService valuationService;
 
 	/*
-	 * @GetMapping("get-data") public String getData() { return proxy.sayHello(); }
+	 * @GetMapping("get-data") public String getData() { return
+	 * proxy.sayHello(); }
 	 */
-//	
-//	@GetMapping("valuation")
-//	public String getValuation() {
-//		return proxy.sayHi();
-//	}
-//	
+	//
+	// @GetMapping("valuation")
+	// public String getValuation() {
+	// return proxy.sayHi();
+	// }
+	//
 
 	@PostMapping("/valuation")
 	ResponseEntity<ValuationResponseDto> postValuation(@RequestBody ValuationRequestDto valuationRequestDto) {
@@ -150,7 +145,7 @@ public class MortgageController {
 	@PostMapping("/payment-details")
 	ResponseEntity<PaymentDetailsResponseDto> payemtDetails(
 			@RequestBody @Valid PaymentDetailsRequestDto paymentDetailsRequestDto, Errors errors) {
-		
+
 		if (errors.hasErrors()) {
 			throw new InvalidInputException("Invalid Input.");
 		}
